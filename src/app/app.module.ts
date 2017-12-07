941d9d7eae1befcfbdf744041fabdbc1826fe56d
Rahul Mundhe <rahulm@zedo.com>
2017-12-07 14:52:02 +0530

    added materail demo componenet and dependency installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StringComponentComponent } from './string-component/string-component.component';
 import { ArrayComponenetComponent } from './array-componenet/array-componenet.component';
+import { MaterialDemoComponent } from './material-demo/material-demo.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StringComponentComponent,
-    ArrayComponenetComponent
+    ArrayComponenetComponent,
+    MaterialDemoComponent
   ],
   imports: [
     BrowserModule
